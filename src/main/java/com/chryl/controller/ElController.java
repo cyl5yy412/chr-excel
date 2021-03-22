320cfbd5713c5fa81b38151457786918e365be19
chryl <cyl5yy412@gmail.com>
2021-03-22 14:52:04 +0800

    eladmin 导出

--- src/main/java/com/chryl/controller/ElController.java
+++ src/main/java/com/chryl/controller/ElController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -39,6 +40,44 @@ public class ElController {
         Map<String, Object> map2 = JSON.parseObject(JSON.toJSONString(j2), Map.class);
         objList.add(map1);
         objList.add(map2);
+        FileUtil.downloadExcel(objList, response);
+
+
+    }
+
+
+    /**
+     * !!!!!!!!!!!!!!! okokokok
+     *
+     * @param response
+     * @throws IOException
+     */
+    @GetMapping("/excel222")
+    public void show222(HttpServletResponse response) throws IOException {
+
+        List<Map<String, Object>> objList = new ArrayList<>();//导出的list map
+
+        List<LoanInfo> loanInfoList = new ArrayList<>();
+        LoanInfo j1 = new LoanInfo();
+        LoanInfo j2 = new LoanInfo();
+        j1.setEmpId("123id");
+        j1.setEmpCode("123code");
+        j1.setEmpName("123name");
+        j2.setEmpCode("124code");
+        j2.setEmpName("124name");
+        loanInfoList.add(j1);
+        loanInfoList.add(j2);
+
+
+        for (LoanInfo loanInfo : loanInfoList) {
+            Map<String, Object> map = new LinkedHashMap<>();
+            map.put("职工ID编号", loanInfo.getEmpId());
+            map.put("职工名", loanInfo.getEmpName());
+            map.put("职工编号", loanInfo.getEmpCode());
+            map.put("STR日期", loanInfo.getStrDate());
+            map.put("日期", loanInfo.getDate());
+            objList.add(map);
+        }
         FileUtil.downloadExcel(objList, response);
 
 
